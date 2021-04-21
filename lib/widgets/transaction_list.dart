5cdb02087d170aa3f6548f7327be843b6e94fd5a
unknown <moonlinux@yahoo.com>
2021-04-21 12:12:13 +0200

    Adding ListView.Builder

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -11,43 +11,52 @@ class TransactionList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-        child: Column(
-            children: transactions.map((e) {
-      return Card(
-        child: Row(
-          children: <Widget>[
-            Container(
-              padding: EdgeInsets.all(10),
-              margin: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
-              decoration: BoxDecoration(
-                border: Border.all(
-                    width: 2, style: BorderStyle.solid, color: Colors.purple),
-                borderRadius: BorderRadius.all(
-                  Radius.circular(10),
-                ),
+      height: 300,
+      child: ListView.builder(
+          itemBuilder: (ctx, index) {
+            return Card(
+              child: Row(
+                children: <Widget>[
+                  Container(
+                    padding: EdgeInsets.all(10),
+                    margin: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                          width: 2,
+                          style: BorderStyle.solid,
+                          color: Colors.purple),
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(10),
+                      ),
+                    ),
+                    child: Text(
+                      '\$ ${transactions[index].amount.toString()}',
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20,
+                          color: Colors.purple),
+                    ),
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text(
+                        transactions[index].title,
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 15),
+                      ),
+                      Text(
+                          DateFormat()
+                              .add_yMMMMd()
+                              .format(transactions[index].date),
+                          style: TextStyle(color: Colors.grey, fontSize: 13))
+                    ],
+                  )
+                ],
               ),
-              child: Text(
-                '\$ ${e.amount.toString()}',
-                style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 20,
-                    color: Colors.purple),
-              ),
-            ),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                Text(
-                  e.title,
-                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 15),
-                ),
-                Text(DateFormat().add_yMMMMd().format(e.date),
-                    style: TextStyle(color: Colors.grey, fontSize: 13))
-              ],
-            )
-          ],
-        ),
-      );
-    }).toList()));
+            );
+          },
+          itemCount: transactions.length),
+    );
   }
 }
